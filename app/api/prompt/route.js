c921ff93cbfab574cac88720bfbef450ce4b3aba
Shahryar <shahryarrauf697@gmail.com>
2023-08-25 01:53:23 +0500

    fix fetching all prompts

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -4,7 +4,7 @@ import Prompt from '@models/prompt'
 export const GET = async (request) => {
     try {
         await connectToDB();
-        const prompts = await Prompt.find({}).populate('creator').exec();
+        const prompts = await Prompt.find().populate('creator').exec();
 
         return new Response(JSON.stringify(prompts), {
             status: 200
